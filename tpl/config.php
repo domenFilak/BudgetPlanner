68474eb80e9266d6762d63cc002ba73051e721cb
domenFilak <106601742+domenFilak@users.noreply.github.com>
2023-06-15 15:00:06 +0200

    Delete config.php
    deleting config.php file - must be added for conn to db

--- tpl/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$config["dbUserName"] = "4055721_mojabaza";
-	$config["dbPassword"] = "Domen1234@";
-	$config["dbHostName"] = "fdb32.awardspace.net";
-	$config["dbName"] = "4055721_mojabaza";
-
-?>
